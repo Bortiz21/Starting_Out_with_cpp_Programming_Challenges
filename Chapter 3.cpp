3cdd26754267c7462cce4d6b0f293dfa69b4a583
Brandon O <116747639+Bortiz21@users.noreply.github.com>
2023-01-08 22:47:20 -0500

    Chapter 3 commit
    Solutions to chapter 3

---
+++ Chapter 3.cpp
@@ -0,0 +1,97 @@
+//
+// Created by brand on 1/8/2023.
+//
+//#include <iostream>
+//#include <iomanip>
+//#include <string>
+//using namespace std;
+//
+//int main(){
+//////    1. Miles per gallon
+////    int gal_size,full_tank_mileage,range;
+////
+////    cout << "This program will calculate your car's miles" << endl;
+////    cout << "To begin, please input your cars Gallon size :";
+////    cin >> gal_size;
+////    cout << "Please input how many miles it can drive on a full tank of gas :";
+////    cin >> full_tank_mileage;
+////
+////    range = full_tank_mileage / gal_size;
+////
+////    cout << "\nYour Miles Per gallon is :" << range;
+//
+//////    2. Stadium seating
+////    const float classA_price (15), classB_price(12), classC_price(9);
+////
+////    float classA_sold, classA_total, classB_sold, classB_total, classC_sold, classC_total, grand_total;
+////    cout << "\nThis program will calculate your sales for each game in your stadium" << endl;
+////    cout << "To Begin please input the amount of tickets sold per class seats" << endl;
+////    cout << "------------------------------------------------------------------- " << endl;
+////
+////    cout << "Class A :";
+////     cin >> classA_sold;
+////    cout << "Class B :";
+////     cin >> classB_sold;
+////    cout << "Class C :";
+////     cin >> classC_sold;
+////
+////    classA_total = classA_sold * classA_price;
+////    classB_total = classB_sold * classB_price;
+////    classC_total = classC_sold * classC_price;
+////    grand_total = classA_total + classB_total + classC_total;
+////    cout << setprecision(2) << fixed;
+////    cout << "\tYour total revenue for seats is :$" << grand_total;
+////
+////
+//
+//////    3. Test Average
+////    const int five(5);
+////    float t1,t2,t3,t4,t5, average;
+////    cout << "Input five test scores and recive an average :";
+////    cin >> t1 >> t2 >> t3 >> t4 >> t5;
+////
+////    average = (t1 + t2 + t3 + t4 + t5) / five;
+////
+////    cout << setprecision(1) << fixed << endl;
+////    cout << "Your test score average is :" << average;
+//
+//////    4. Average Rainfall
+////    float  m1r, m2r, m3r, average;
+////    string m1, m2 , m3;
+////    cout << "Input three months";
+////    cin >> m1 >> m2 >> m3;
+////
+////
+////    cout << "\nInput the amount of rain per month (In inches)" << endl;
+////    cout << m1 << " :";
+////    cin >> m1r;
+////    cout << m2 << " :";
+////    cin >> m2r;
+////    cout << m3 << " :";
+////    cin >> m3r;
+////
+////    average = ( m1r + m2r + m3r) / 3;
+////    cout << setprecision(2) << fixed<< endl;
+////
+////    cout << "The average rain fall in the months of " << m1 << ", " << m2 << ", & " << m3 << " is :" << average << " inches.";
+//
+//////    5. Male and Female Percentages
+////    float males, females, male_percentage, female_percentage, total_students;
+////    cout << "How many males are in your class ? :";
+////    cin >> males;
+////    cout << "How many Females are in your class? :";
+////    cin >> females;
+////    total_students = males + females;
+////
+////    //figure out the percentage of males & females
+////    male_percentage = males / total_students;
+////    female_percentage = females / total_students;
+////
+////    cout << setprecision(2) << fixed;
+////
+////    cout << "Your class consists of -" << endl;
+////    cout << "====================" << endl;
+////    cout << male_percentage << "% of males." << endl;
+////    cout << female_percentage << "% of females." << endl;
+//
+//}
---
+++ Chapter2.cpp
@@ -0,0 +1,3 @@
+//
+// Created by brand on 1/8/2023.
+//
